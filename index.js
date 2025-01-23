2ee879bfc01945c3ac6ad3ca5fcd7230c6af2620
Jean Pierre CORDEIRO <62947268+JeanPCordeiro@users.noreply.github.com>
2025-01-23 16:35:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ try {
     app.get('/', (req, res) => {
 
         res.send (`
-        Lean Systems PAAS v2<p>
+        Lean Systems PAAS v2.1<p>
         <a href="/sendtolog?message=hello">send a message to the log</a><p>
         hostname : ${hostname}<p>
         uptime : ${uptime}<p>
